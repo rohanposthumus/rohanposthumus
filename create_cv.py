b2c81233af36978b6209958dae4c574a5ba41472
rohanposthumus <rohanposthumus@gmail.com>
2025-10-22 16:04:16 +0200

    Added script for auto CV

---
+++ create_cv.py
@@ -0,0 +1,9 @@
+import sys
+import re
+from bs4 import BeautifulSoup
+from reportlab.lib.pagesizes import A4
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, PageBreak
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import cm
+from reportlab.lib.colors import HexColor
+from reportlab.lib.enums import TA_LEFT, TA_CENTER
